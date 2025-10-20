dc12d0544420bd12471caca6c94a473f5cd049a6
Tomasz Janicki <tomasz.janicki@inetum.com>
2025-10-20 21:33:06 +0200

    x

---
+++ Performance/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Performance;
+using Serilog;
+
+var configuration = GetConfiguration();
+var serviceProvider = GetServiceProvider(configuration);
+var testService = serviceProvider.GetRequiredService<TestService>();
+testService.DoSth();
+
+return;
+
+static IConfiguration GetConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true);
+
+    return builder.Build();
+}
+
+static IServiceProvider GetServiceProvider(IConfiguration configuration)
+{
+    var services = new ServiceCollection();
+    services.AddSingleton<TestService>();
+    services.AddLogging();
+    services.AddSerilog(cfg => cfg.ReadFrom.Configuration(configuration));
+
+    return services.BuildServiceProvider();
+}
---
+++ Performance/TestService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Performance;
+
+public sealed class TestService(ILogger<TestService> logger)
+{
+    public void DoSth() => 
+        logger.LogInformation("DoSth");
+}
